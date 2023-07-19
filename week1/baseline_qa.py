8aa50e5403f35d6d46015777c7aaf9e969a582f2
강지혁 <wlgur7238@snu.ac.kr>
2023-07-19 21:00:48 +0900

    filter meaningless questions with another embedding

--- week1/baseline_qa.py
+++ week1/baseline_qa.py
@@ -9,6 +9,8 @@ from annoy import AnnoyIndex
 import os
 import dotenv
 
+from index import build_index, is_meaningless_query
+
 dotenv.load_dotenv()
 openai.api_key = os.getenv('API_KEY')
 
@@ -17,33 +19,27 @@ with open(Path(__file__).resolve().parent / "openai27052023.yaml", 'r') as f:
     paper = yaml.safe_load(f)
 sentences = paper['sentences']
 
-
-# --- embed chunks --- #
-embeddings = [
-    r['embedding']
-    for r in openai.Embedding.create(input=sentences, model='text-embedding-ada-002')['data']
-] 
-
-# --- index embeddings for efficient search (using Spotify's annoy)--- #
-hidden_size = len(embeddings[0])
-index = AnnoyIndex(hidden_size, 'angular')  #  "angular" =  cosine
-for i, e in enumerate(embeddings): 
-    index.add_item(i , e)
-index.build(10)  # build 10 trees for efficient search
+# --- build index with given pdf sentences --- #
+index = build_index(sentences)
 
 messages = [
-        {"role": "system", "content": 'You are a helpful assistant.'}
-    ]
+    {"role": "system", "content": 'You are a helpful assistant.'}
+]
 
 # --- iteratively answer questions (retrieve & generate) --- #
 while True:
     query = input("Your question: ")
-    embedding =  openai.Embedding.create(input = [query], model='text-embedding-ada-002')['data'][0]['embedding']
+    embedding = openai.Embedding.create(input=[query], model='text-embedding-ada-002')['data'][0]['embedding']
+
+    # filter out meaningless questions (e.g., "How are you?")
+    if is_meaningless_query(embedding):
+        continue
+
     # get nearest neighbors by vectors
     indices, distances = index.get_nns_by_vector(embedding,
-                                                  n=3,  # return top 3
-                                                  include_distances=True)
-    results =  [ 
+                                                 n=3,  # return top 3
+                                                 include_distances=True)
+    results = [
         (sentences[i], d)
         for i, d in zip(indices, distances)
     ]
@@ -64,15 +60,12 @@ while True:
     In your answer, make sure to cite the excerpts by its number wherever appropriate.
     Note, however, that the excerpts may not be relevant to the user query.
     """
-    
-    messages += [{"role":"user", "content": query}]
+
+    messages += [{"role": "user", "content": query}]
     chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                    messages=messages)
+                                                   messages=messages)
     answer = chat_completion.choices[0].message.content
-    
-    messages += [{"role":"system", "content":answer}]
+
+    messages += [{"role": "system", "content": answer}]
     answer += f"\n--- EXCERPTS ---\n{excerpts}"
     print(answer)
-
-
-
